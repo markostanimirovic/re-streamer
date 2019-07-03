019bab867e33532a7d84abdafb730345aa1bbb0e
stanimirovic <markostanimirovic95@gmail.com>
2019-07-03 22:51:54 +0200

    Created state-stream and stream functions

--- src/re_streamer/core.clj
+++ src/re_streamer/core.clj
@@ -1,7 +1,20 @@
-(ns re-streamer.core
-  (:gen-class))
+(ns re-streamer.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn state-stream [initial-value]
+  (let [callbacks (atom [])
+        value (atom initial-value)]
+    {:subscribe! (fn [callback]
+                   (do (swap! callbacks conj callback)
+                       (callback @value)))
+     :emit!      (fn [new-value]
+                   (do (reset! value new-value)
+                       (doseq [callback @callbacks] (callback @value))))}))
+
+(defn stream []
+  (let [callbacks (atom [])
+        value (atom nil)]
+    {:subscribe! (fn [callback]
+                   (do (swap! callbacks conj callback)))
+     :emit!      (fn [new-value]
+                   (do (reset! value new-value)
+                       (doseq [callback @callbacks] (callback @value))))}))
