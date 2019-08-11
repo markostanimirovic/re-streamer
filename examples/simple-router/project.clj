e281c750d8bf0a2a12e5886a33d2a94ee58c5e63
stanimirovic <markostanimirovic95@gmail.com>
2019-08-11 15:36:37 +0200

    Updated dependencies in example projects.

--- examples/simple-router/project.clj
+++ examples/simple-router/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
-                 [org.clojars.stanimirovic/re-streamer "0.3.6"]]
+                 [org.clojars.stanimirovic/re-streamer "0.3.9"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.19"]]
